422e57b150b580ae433c5f39d299d5c380f66ae5
RhysRoo <93945223+RhysRoo@users.noreply.github.com>
2024-05-05 19:12:04 +0100

    magic_8_ball

---
+++ magic_8_ball.py
@@ -0,0 +1,11 @@
+import random
+
+question = input('What is your question? ')
+
+outcomes = {1:'definitely', 2:'It is decidedly so', 3:'Without a doubt', 4:'Reply hazy, try again', 5: 'Ask again later.', 6:'Better not tell you now', 7: 'My sources say no.', 8: 'Outlook not so good.', 9: 'Outlook not so good.', 10: 'Very doubtful.'}
+
+Magic_8_Ball = random.randint(0,10)
+
+output = outcomes.get(Magic_8_Ball, 'Null')
+
+print(output)
